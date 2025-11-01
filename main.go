93f10a5caa42fd490752391637cf620b417a52d7
Bas S.H.T. Michielsen <github@bshtmichielsen.eu>
2025-11-01 10:16:33 +0100

    moved animals to Eindhoven

--- main.go
+++ main.go
@@ -9,7 +9,9 @@ import (
 
 const addr = ":8080"
 
-var centralPoint = Point{Latitude: 52.088644, Longitude: 5.172076} // Botanische Tuinen
+// var centralPoint = Point{Latitude: 52.088644, Longitude: 5.172076} // Botanische Tuinen
+
+var centralPoint = Point{Latitude: 51.426091, Longitude: 5.482524} // Stadswandelpark Eindhoven
 
 func main() {
 	apiURL := os.Getenv("API_URL")
